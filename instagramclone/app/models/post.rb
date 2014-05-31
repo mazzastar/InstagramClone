740d56310307696c4cec2fe0273fbe02ffceef1d
Alexander Wong Makers <mavrm@hotmail.com>
2014-05-31 18:08:03 +0100

    correct CSS and tags input

--- instagramclone/app/models/post.rb
+++ instagramclone/app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
 
 
-	has_attached_file :picture , styles: {medium: '300x300>'}
+	has_attached_file :picture , styles: {medium: '300x300>'}, :default_url => "Ghost.png"
 	validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
 	belongs_to :user
 	has_and_belongs_to_many :hashtags, uniq: true
