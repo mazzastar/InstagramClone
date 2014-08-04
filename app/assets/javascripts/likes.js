fd91db73eb02bf3f45e6e59790d95d2a307e13f3
Alexander Wong Makers <mavrm@hotmail.com>
2014-08-04 16:27:23 +0100

    add address

--- app/assets/javascripts/likes.js
+++ app/assets/javascripts/likes.js
@@ -1,73 +1,74 @@
 $(document).ready(function(){
-	$('.btn-like').on('click', function(event){
-		console.log($(this).closest('a')[0]);
-		var x = $(this).closest('.btn-like');
-		//console.log(x.attr('data-method'));
-
+	$('body').on('click', '.btn-like', function(event){
 		event.preventDefault();
-		var button = this;
-		$.post(this.href, function(result){
-			// button.toggleClass('btn-like', 'btn-unlike');
-			// console.log(result);
-			// console.log(result.new_like_count);
-			var $response=$(result);
-
-    		//Query the jQuery object for the values
-   	//	 	var oneval = $response.filter('new_like_count');
-   		 	//console.log(oneval);
-    		// var subval = $response.filter('#sub').text();
-		});
-		return false;   
-
-
-	});	
-
-	$('.btn-unlike').on('click', function(event){
-		event.preventDefault();
-		 var x = ($(this).closest('.btn-unlike').removeClass('btn-unlike').addClass('btn-like'));
-		 console.log(x);
-		  // console.log(x.attr('data-method'));
-		 // console.log(x.attr('href'))
-		 // console.log(x.attr('href').match(/(\/\w*\/[0-9]+\/likes)/)[0]);
-				 // console.log(x);
-		 // console.log($(this).closest('.btn-like'));
-		
-		// $.ajax({
-  //   		url: this.href,
-  //   		type: 'DELETE',
-  //   		 success: function(result) {
-  //       		console.log("Deleting Attempt");
-  //   		}
-		// });
-		// console.log($(this).parent);
-
-		//	<a class="btn btn-lg btn-default btn-like" href="/posts/34/likes">♥ 0</a>
-		//  <a class="btn btn-lg btn-default btn-unlike" data-method="delete" href="/posts/34/likes/487" rel="nofollow">♥ 9</a>
-
-		$.ajax({
-			url: this.href,
-			type: "post",
-			dataType: "json",
-			data: {"_method": "delete"},
-			success: function(result) {
-        		// console.log("Deleting Attempt");
-        		// console.log(result);
-        		x.removeAttr('data-method');
-        		x.removeAttr('rel');
-		 		x.attr('href', x.attr('href').match(/(\/\w*\/[0-9]+\/likes)/)[0] )
-
-    		}
-		});
-		return false;   
+		$.post(this.href);
+		console.log(this.href);
+		$(this).removeClass('btn-like').addClass('btn-unlike');
 	});
 
-	var connection = new WebSocketRails(window.location.host + '/websocket');
+	$('body').on('click', '.btn-unlike', function(event){
+		event.preventDefault();
+		$.ajax({url: this.href, type: 'delete'});
+		$(this).removeClass('btn-unlike').addClass('btn-like');
+	});
+
+	var connection = new WebSocketRails(window.location.host+'/websocket');
 	channel = connection.subscribe('likes');
+
 	channel.bind('new', function(post){
-		var postElement = $('.post[data-id='+post.id+ '] .btn-like')
-		postElement.text('♥ '+post.new_like_count);
-	
-		var postElement2 = $('.post[data-id='+post.id+ '] .btn-unlike')
-		postElement2.text('♥ '+post.new_like_count);
+		var postElem = $('.post[data-id='+post.id+'] .btn-unlike');
+		postElem.text('♥ '+post.new_like_count);
 	});
+
+	channel.bind('destroy', function(post){
+		var postElem = $('.post[data-id='+post.id+'] .btn-like');
+		postElem.text('♥ '+post.new_like_count);
+	});
+
 })
+
+
+// $(document).ready(function(){
+// 	$('.btn-like').on('click', function(event){
+// 		console.log($(this).closest('a')[0]);
+// 		var x = $(this).closest('.btn-like');
+// 		event.preventDefault();
+// 		var button = this;
+// 		$.post(this.href, function(result){			
+// 			var $response=$(result);
+// 		});
+// 		return false;   
+
+
+// 	});	
+
+// 	$('.btn-unlike').on('click', function(event){
+// 		event.preventDefault();
+// 		 var x = ($(this).closest('.btn-unlike').removeClass('btn-unlike').addClass('btn-like'));
+// 		$.ajax({
+// 			url: this.href,
+// 			type: "post",
+// 			dataType: "json",
+// 			data: {"_method": "delete"},
+// 			success: function(result) {
+//         		// console.log("Deleting Attempt");
+//         		// console.log(result);
+//         		x.removeAttr('data-method');
+//         		x.removeAttr('rel');
+// 		 		x.attr('href', x.attr('href').match(/(\/\w*\/[0-9]+\/likes)/)[0] )
+
+//     		}
+// 		});
+// 		return false;   
+// 	});
+
+// 	var connection = new WebSocketRails(window.location.host + '/websocket');
+// 	channel = connection.subscribe('likes');
+// 	channel.bind('new', function(post){
+// 		var postElement = $('.post[data-id='+post.id+ '] .btn-like')
+// 		postElement.text('♥ '+post.new_like_count);
+	
+// 		// var postElement2 = $('.post[data-id='+post.id+ '] .btn-unlike')
+// 		postElement2.text('♥ '+post.new_like_count);
+// 	});
+// })
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,12 @@
 class LikesController < ApplicationController
   def create
   	@post = Post.find params[:post_id]
-  	@post.likes.create(user_id: current_user.id)
-  	WebsocketRails[:likes].trigger 'new', {id: @post.id, new_like_count: @post.likes.count, like_id: @post.likes.last.id}
+  	@like = @post.likes.create(user_id: current_user.id)
+  	# WebsocketRails[:likes].trigger 'new', {id: @post.id, new_like_count: @post.likes.count, like_id: @post.likes.last.id}
+    WebsocketRails[:likes].trigger 'new', {id: @post.id, new_like_count: @post.likes.count, like_id: @like.id}
+    render nothing: true
 
-  	 redirect_to '/posts'
+  	 # redirect_to '/posts'
   end
 
 
@@ -19,9 +21,10 @@ class LikesController < ApplicationController
   	@post = Post.find(params[:post_id])
   	@link = @post.likes.find_by(user_id: current_user.id)
   	@link.destroy
-  	WebsocketRails[:likes].trigger 'new', {id: @post.id, new_like_count: @post.likes.count}
+  	WebsocketRails[:likes].trigger 'destroy', {id: @post.id, new_like_count: @post.likes.count}
+    render nothing: true
 
-  	redirect_to '/posts'
+  	# redirect_to '/posts'
   end
 end
 
